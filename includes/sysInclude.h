b1a6bdd7e7b7b69914fb7d4bd856374655053bcb
jure <jure@mocnik.si>
2020-09-05 16:47:11 +0200

    Moved system included files to sysInclude.h

---
+++ includes/sysInclude.h
@@ -0,0 +1,44 @@
+/*
+ * sysInclude.h
+ *
+ *  Created on: Sep 5, 2020
+ *      Author: Jure
+ */
+
+#ifndef SYSINCLUDE_H_
+#define SYSINCLUDE_H_
+
+/* required DSP56F800E_Quick_Start header */
+#include "qs.h"
+
+/* low-level driver headers for each module used */
+#include "occs.h"
+#include "sys.h"
+#include "adc12.h"
+#include "adc16.h"
+#include "aoi.h"
+#include "cop.h"
+#include "crc.h"
+#include "dac.h"
+#include "dma.h"
+#include "fcan.h"
+#include "fmc.h"
+#include "ftfl.h"
+#include "gpio.h"
+#include "hscmp.h"
+#include "iic.h"
+#include "intc.h"
+#include "intctarget.h"
+#include "mcm.h"
+#include "pmc.h"
+#include "pwm.h"
+#include "pit.h"
+#include "qtimer.h"
+#include "sci.h"
+#include "scihl.h"
+#include "spi.h"
+#include "xbar.h"
+#include "pdb.h"
+#include "freemaster.h"
+
+#endif /* SYSINCLUDE_H_ */
--- main.c
+++ main.c
@@ -9,38 +9,7 @@
  *
  *******************************************************************************/
 
-/* required DSP56F800E_Quick_Start header */
-#include "qs.h"
-
-/* low-level driver headers for each module used */
-#include "occs.h"
-#include "sys.h"
-#include "adc12.h"
-#include "adc16.h"
-#include "aoi.h"
-#include "cop.h"
-#include "crc.h"
-#include "dac.h"
-#include "dma.h"
-#include "fcan.h"
-#include "fmc.h"
-#include "ftfl.h"
-#include "gpio.h"
-#include "hscmp.h"
-#include "iic.h"
-#include "intc.h"
-#include "intctarget.h"
-#include "mcm.h"
-#include "pmc.h"
-#include "pwm.h"
-#include "pit.h"
-#include "qtimer.h"
-#include "sci.h"
-#include "scihl.h"
-#include "spi.h"
-#include "xbar.h"
-#include "pdb.h"
-#include "freemaster.h"
+#include "sysInclude.h"
 
 #include "functions.h"
 
